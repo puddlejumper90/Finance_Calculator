85a8dbda4edc49c459bd04419e84762fc63477b3
puddlejumper90 <55165215+puddlejumper90@users.noreply.github.com>
2020-03-24 09:02:09 -0500

    Feature Update
    1. Added import os and csv libraries
    2. Added "LIST_OF_ENTRIES" variable for FC_Log.csv file function
    3. Added "user" variable to pull in username for logging purposes - WIP
    4. "search_and_append" function is a WIP. I have created a test .py file that will allow me to experiment with this function alone. I will add to the "Finance_Calculator.py" application once I have perfected the code. The goal is to search the cd for "FC_Log.csv" and create the file if it doesn't exist, then start adding new lines of data to the log. If the file does exist, the application will append the file by adding a new line to the log file.
    
    ***Any suggestions for this function would be greatly appreciated.

--- Finance_Calculator.py
+++ Finance_Calculator.py
@@ -4,6 +4,8 @@ import scipy as sp
 import pandas as pd
 import matplotlib as mb
 import datetime
+import os
+import csv
 
 #Greeting Message
 print("Welcome you your personal finance calculator! :-)")
@@ -11,14 +13,18 @@ time1 = datetime.datetime.now()
 print("CURRENT TIME:", time1)
 current_time = time1
 print("------------------------------------------------------------------------")
-#Inserts two blank lines so that the application is easier to read for the user
-def TBL():
+
+#Defined Functions
+
+def TBL(): #Inserts two blank lines so that the application is easier to read
     print("")
     print("")
 
 TBL()
 
-#Input values
+#Input values and variables
+user = x
+filename = "FC_Log.csv"
 y_salary = float(input("Input salary, no special characters are needed:"))
 TBL()
 y_tax = float(input("Input expected tax rate as a decimal number (a percentage):"))
@@ -53,6 +59,24 @@ print("Your yearly savings should be: $", savings_calc)
 TBL()
 print("If you save at this rate for 30 years, you will have: $", thirty_year_savings, "If you find a way to cut your monthly costs, more money will be available for savings!")
 TBL()
+print("THANK YOU FOR USING THE FINANCE CALCULATOR: We are looking to add more features to this application in the near future!")
 
-#Values to be added to application .csv file
 LIST_OF_VALUES = [current_time, user, y_salary, y_tax, salary_after_tax, monthly_pay, bi_weekly_pay, savings, savings_calc, salary_after_savings, monthly_savings, monthly_expenses, left_over]
+LIST_OF_ENTRIES =["current_time":current_time, "user":user, "y_salary":y_salary, "y_tax":y_tax, "salary_after_tax":salary_after_tax, "monthly_pay":monthly_pay, "bi_weekly_pay":bi_weekly_pay, "savings":savings, "savings_calc":savings_calc, "salary_after_savings":salary_after_savings, "monthly_savings":monthly_savings, "monthly_expenses":monthly_expenses, "left_over":left_over]
+
+'''
+#Search for "FC_Log.csv", create file if it does not exist, else add  "LIST_OF_VALUES" to the next open 
+line
+def search_and_append():
+    with open("FC_Log.csv"):
+        if not "FC_Log.csv":
+            with open("FC_Log.csv", 'w', newline="LIST_OF_VALUES") as f:
+                write_csv = csv.writer(f)
+
+                write_csv.writerow(LIST_OF_ENTRIES)
+
+        else with open ("FC_Log.csv", "rb") as f:
+            write = write_csv(f)
+            write.writerow(LIST_OF_ENTRIES)
+
+'''
