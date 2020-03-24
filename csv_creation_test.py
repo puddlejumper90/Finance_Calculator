aa8ea72eb0cf336df73999dcbc55c14c79bf7256
puddlejumper90 <55165215+puddlejumper90@users.noreply.github.com>
2020-03-24 14:02:48 -0500

    Final Test Update for .csv file creation
    This update is based on the data that is actually in the final application. The basic function starts from "with open."

--- csv_creation_test.py
+++ csv_creation_test.py
@@ -1,5 +1,7 @@
 import csv
 
+LIST_OF_VALUES = ["current_time", "user", "y_salary", "y_tax", "salary_after_tax", "monthly_pay", "bi_weekly_pay", "savings", "savings_calc", "salary_after_savings", "monthly_savings", "monthly_expenses", "left_over"]
+
 with open("FC_Log.csv", "w+", newline="") as file:
     writer = csv.writer(file)
-    writer.writerow([1,2,3,4,5,6,7])
+    writer.writerow(LIST_OF_VALUES)
