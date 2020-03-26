919bb930d36aca7f0de90a70f59e78f4cfdad492
puddlejumper90 <55165215+puddlejumper90@users.noreply.github.com>
2020-03-26 10:14:34 -0500

    Development Tweaks
    1. "username" can now be imported
    2. Removed "LIST_OF_ENTRIES" for the time being for further development

--- Finance_Calculator.py
+++ Finance_Calculator.py
@@ -6,6 +6,7 @@ import matplotlib as mb
 import datetime
 import os
 import csv
+import getpass
 
 #Greeting Message
 print("Welcome you your personal finance calculator! :-)")
@@ -23,7 +24,7 @@ def TBL(): #Inserts two blank lines so that the application is easier to read
 TBL()
 
 #Input values and variables
-user = x
+user = getpass.getuser()
 filename = "FC_Log.csv"
 y_salary = float(input("Input salary, no special characters are needed:"))
 TBL()
@@ -48,9 +49,11 @@ monthly_expenses = float(input("Input your total monthly expenses as a decimal n
 TBL()
 
 left_over = float(monthly_pay - (savings + monthly_expenses))
-#thirty_year_savings = float(savings_calc * 30)
+thirty_year_savings = float(savings_calc * 30)
 
 #Application outputs
+print("----------------------------------------------------------------------------")
+TBL()
 print("Your left over money each month should be: $", left_over)
 TBL()
 print("Amount put into savings each month: $", monthly_savings)
@@ -62,11 +65,9 @@ TBL()
 print("THANK YOU FOR USING THE FINANCE CALCULATOR: We are looking to add more features to this application in the near future!")
 
 LIST_OF_VALUES = [current_time, user, y_salary, y_tax, salary_after_tax, monthly_pay, bi_weekly_pay, savings, savings_calc, salary_after_savings, monthly_savings, monthly_expenses, left_over]
-LIST_OF_ENTRIES =["current_time":current_time, "user":user, "y_salary":y_salary, "y_tax":y_tax, "salary_after_tax":salary_after_tax, "monthly_pay":monthly_pay, "bi_weekly_pay":bi_weekly_pay, "savings":savings, "savings_calc":savings_calc, "salary_after_savings":salary_after_savings, "monthly_savings":monthly_savings, "monthly_expenses":monthly_expenses, "left_over":left_over]
-
 
 #Search for "FC_Log.csv", create file if it does not exist, else add  "LIST_OF_VALUES" to the next open line
 def search_and_append():
     with open("FC_Log.csv", "w+", newline="") as file:
-    writer = csv.writer(file)
-    writer.writerow(LIST_OF_VALUES)
+        writer = csv.writer(file)
+        writer.writerow([List_of_Values])
