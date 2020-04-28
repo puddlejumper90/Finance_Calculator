145712fcb28ee6e47968d15c533e3079966d8292
puddlejumper90 <55165215+puddlejumper90@users.noreply.github.com>
2020-04-28 13:01:20 -0500

    Added update notes
    Notes are located at the bottom of the code.

--- csv_creation_test.py
+++ csv_creation_test.py
@@ -5,3 +5,7 @@ LIST_OF_VALUES = ["current_time", "user", "y_salary", "y_tax", "salary_after_tax
 with open("FC_Log.csv", "w+", newline="") as file:
     writer = csv.writer(file)
     writer.writerow(LIST_OF_VALUES)
+'''
+This does work; however, I am working on a way for the application to search for the appropriate file in the current directory,
+and add a row to it if it exists or create the appropriate file and then add a new row to the new file. More to follow!
+'''
