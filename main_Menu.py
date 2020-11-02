511a1042cf194e9a1474474cd0bb1ae99b81819d
puddlejumper90 <55165215+puddlejumper90@users.noreply.github.com>
2020-11-02 14:44:17 -0600

    Established new file
    Began work on main menu

---
+++ main_Menu.py
@@ -0,0 +1,17 @@
+#Import needed libraries
+import numpy as np
+import pandas as pd
+import pandasql as psql
+
+
+class app_func():
+    
+    def main_menu():
+        print('Thank you for using the financial calculator, by Puddlejumper90')
+        print('')
+        print('===PLEASE SELECT ON OF THE FOLLOWING OPTIONS:===')
+        print('')
+        print('1. Finance Calculator')
+        print('2. Dividend Income Calculator')
+        print('3. Help')
+        print('4. Exit')
