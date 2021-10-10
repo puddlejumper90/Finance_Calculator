6efaea4e51e79f0262835be74ae95c2e8975a8f7
puddlejumper90 <55165215+puddlejumper90@users.noreply.github.com>
2021-10-10 08:55:56 -0500

    Menu connection
    This file can now access Financial_Calculator.py application.

--- main_Menu.py
+++ main_Menu.py
@@ -2,8 +2,8 @@
 import numpy as np
 import pandas as pd
 import pandasql as psql
-
-
+import time
+#import os
 class app_func():
     
     def main_menu():
@@ -15,3 +15,33 @@ class app_func():
         print('2. Dividend Income Calculator')
         print('3. Help')
         print('4. Exit')
+        print('')
+        print('==================================================')
+        #Create menu selection input options
+        menu_select = str(input('Please select an option: '))
+        #Create if statements for each menu option
+        if menu_select == '1':
+            print('You have selected the finance calculator')
+            #open Finance_Calculator.py
+            import Finance_Calculator
+            Finance_Calculator.Finance_Calculator()
+            time.sleep(1)
+        elif menu_select == '2':
+            print('You have selected the dividend income calculator')
+            time.sleep(1)
+
+        elif menu_select == '3':
+            print('You have selected the help menu')
+            time.sleep(1)
+        elif menu_select == '4':
+            print('You have selected to exit the program')
+            time.sleep(1)
+            exit()
+        else:
+            print('You have selected an invalid option')
+            time.sleep(1)
+            app_func.main_menu()
+
+
+
+app_func.main_menu()
