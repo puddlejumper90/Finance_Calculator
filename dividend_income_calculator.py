56208f868b578b6ebff941b7ee56c9132948dbf8
puddlejumper90 <55165215+puddlejumper90@users.noreply.github.com>
2023-07-02 18:31:01 -0500

    Update dividend_income_calculator.py
    Improved code, and added input validation.

--- dividend_income_calculator.py
+++ dividend_income_calculator.py
@@ -1,32 +1,51 @@
-#Import the needed libraries
-import numpy as np
-import pandas as pd
-import pandasql as psql
 import time
 
 #Basic application functions
 class basic_func():
     
+    @staticmethod
     def obl(): #prints one blank line
         print("")
         
+    @staticmethod
     def delay1(): #Sleeps application for two seconds
         time.sleep(1)
         
+    @staticmethod
     def rec(): #Shows extra resources
         print('Good resources for stock information include Yahoo Finance and marketbeat.com')
-        
+
 class app_process():
     
-    
+    @staticmethod
+    def validate_float_input(prompt):
+        while True:
+            try:
+                value = float(input(prompt))
+                return value
+            except ValueError:
+                print("Invalid input. Please enter a decimal number.")
+                continue
+                
+    @staticmethod
+    def validate_str_input(prompt):
+        while True:
+            value = input(prompt)
+            if value.isalpha():
+                return value.upper()
+            else:
+                print("Invalid input. Please enter a valid string.")
+                continue
+        
+    @staticmethod
     def user_entries():
-        yearly_income = float(input('Input needed yearly income as a decimal number: '))
+        yearly_income = app_process.validate_float_input('Input needed yearly income as a decimal number: ')
         basic_func.obl()
         basic_func.delay1()
-        dividend = float(input('Enter projected dividend yield as a decimal number: '))
+        dividend = app_process.validate_float_input('Enter projected dividend yield as a decimal number: ')
         basic_func.obl()
         basic_func.delay1()
-        company = input('Please enter the ticker symbol for the company in question: ')
+        company = app_process.validate_str_input('Please enter the ticker symbol for the company in question: ')
         basic_func.obl()
         basic_func.delay1()
         total_investment = float(yearly_income/dividend)
@@ -39,9 +58,6 @@ class app_process():
         basic_func.delay1()
         b_even = float(total_investment/yearly_income)
         print('It will take you', b_even, 'years to break-even on this investment.')
-        
-#User commands
-#resources = basic_func.rec()        
 
 #Application Process Chain
 app_process.user_entries()
