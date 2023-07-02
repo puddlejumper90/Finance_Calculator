6ea025833e3eb5a293e7a6410536b1a331153fda
puddlejumper90 <55165215+puddlejumper90@users.noreply.github.com>
2023-07-02 18:17:32 -0500

    Update Postgres_Table_Creator.sql

--- Postgres_Table_Creator.sql
+++ Postgres_Table_Creator.sql
@@ -1,1 +1,23 @@
+/*
+Puddlejumper90 --- 7/2/2023 --- Establish Financial_Calculator DB creation file.
+*/
 
+--Create a Database
+CREATE DATABASE financial_calculator;
+
+CREATE TABLE personal_financial_scenarios(
+    current_time TIMESTAMP, 
+    user VARCHAR(100),
+    y_salary MONEY, 
+    y_tax FLOAT, 
+    salary_after_tax MONEY, 
+    monthly_pay MONEY, 
+    bi_weekly_pay MONEY, 
+    savings MONEY, 
+    savings_calc MONEY, 
+    salary_after_savings MONEY, 
+    monthly_savings MONEY, 
+    monthly_expenses MONEY, 
+    left_over MONEY, 
+    note TEXT
+)
